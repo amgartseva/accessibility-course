aecc93c90442fb7d6cc3e61d1e7fb6d8ac9df65f
Anastasia Gartseva <anastasia.gartseva@team.wrike.com>
2021-11-06 14:19:37 +0300

    Initional commit 2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# accessibility-course
+# Onboarding task Wrike
+
+To start build you need to run in your terminal:
+
+1) npm i
+2) gulp dev
+
+If you have problems with second step, for example there's an error "command not found: gulp", try to install gulp globally. For that type in your terminal "npm install --global gulp"
---
+++ gulpfile.js
@@ -0,0 +1,151 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const del = require('del');
+const rename = require('gulp-rename');
+const less = require('gulp-less');
+const imagemin = require('gulp-imagemin');
+sass.compiler = require('node-sass');
+const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+const plumber = require('gulp-plumber');
+
+const html = () => {
+  return gulp.src('src/html/*.html')
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
+}
+
+exports.html = html;
+
+const fonts = (done) => {
+  gulp
+    .src('./src/fonts/*{ttf,woff,woff2,svg,eot}')
+    .pipe(gulp.dest('./build/fonts/'));
+  done();
+}
+
+exports.fonts = fonts;
+
+const styles = () => {
+  return gulp
+    .src('./src/less/style.less')
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(
+      less
+        .sync()
+        .on('error', less.logError)
+    )
+    .pipe(gulp.dest('./build/css'))
+    .pipe(
+      less
+        .sync({
+          outputStyle: 'compressed'
+        })
+        .on('error', less.logError)
+    )
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
+}
+
+exports.styles = styles;
+
+const svg_sprite = () => {
+  return gulp
+    .src('./src/svg/*.svg')
+    .pipe(
+      imagemin([
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(gulp.dest('./build/svg/'))
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg'
+          }
+        }
+      })
+    )
+    .pipe(gulp.dest('./build/svg/'));
+}
+
+exports.svg_sprite = svg_sprite;
+
+const images = () => {
+    return gulp
+        .src('./src/img/*.{png,jpg}')
+        .pipe(imagemin([
+            imagemin.optipng({
+                optimizationLevel: 3
+            }),
+            imageminMozjpeg({
+                quality: 90
+            })
+        ]))
+        .pipe(gulp.dest('./build/img'));
+}
+
+exports.images = images;
+
+const js = () => {
+    return gulp
+        .src('./src/js/*.js')
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('build/js'))
+        .pipe(uglify())
+        .pipe(rename('script.min.js'))
+        .pipe(gulp.dest('./build/js'));
+}
+
+exports.js = js;
+
+const clean = () => {
+  return del(['build/*']);
+}
+
+exports.clean = clean;
+
+const watch = () => {
+  browserSync.init({
+    server: {
+      baseDir: './build/'
+    }
+  });
+
+    gulp.watch('./src/scss/**/*.scss', styles);
+    gulp.watch('./src/fonts/**/*.{ttf,woff,woff2,svg,eot}', fonts);
+    gulp.watch('./src/svg/*.svg', svg_sprite);
+    gulp.watch('./src/img/*.{png,jpg}', images);
+    gulp.watch('./src/js/*.js', js);
+    gulp.watch('./src/html/*.html', html);
+}
+
+exports.watch = watch;
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+      html,
+      styles,
+      fonts,
+      svg_sprite,
+      images,
+      js
+  )
+);
+
+exports.build = build;
+
+exports.dev = gulp.series(
+  build,
+  watch
+);
